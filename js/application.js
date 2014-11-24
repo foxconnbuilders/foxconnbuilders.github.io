dc33c9f22f62b43ebabec9746d96f3a5cfb72305
Bryan Chug <bryan.chug@gmail.com>
2014-11-24 15:14:46 +0800

    Initial commit after migrating to github from bitbucket

---
+++ js/application.js
@@ -0,0 +1,47 @@
+var $height = $(window).height();
+var $width = $(window).width();
+
+$(document).ready( function() {
+
+    $('#barousel-foxconn').barousel({
+        navType: 2
+    });
+    $('.barousel_nav').hide();
+
+    $('#carousel-bottom').css('height',($height-80)+"px");
+    $(document).on('click','#learn-button',function(){
+        event.preventDefault();
+        $('html,body').animate({scrollTop: $('.product').offset().top - 80},'slow');
+    });
+});
+
+$(window).on('load',function(){
+    var bHeight = $('.barousel_image').find('.default').css('height');
+    bHeight = parseInt(bHeight);
+    $('.barousel').css('min-height', bHeight+"px");
+    $('.barousel_nav').css('top',0).fadeIn();
+
+    $('.product').fadeIn();
+    $('.project').fadeIn();
+    $('.contact').fadeIn();
+    $('.subMenu').smint({
+        'scrollSpeed' : 1000
+    });
+    $('#nav-bar').fadeIn();
+    $('#learn').fadeIn();
+});
+
+$(function() {
+    $('a[href*=#]:not([href=#carousel-bottom])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top-70
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
